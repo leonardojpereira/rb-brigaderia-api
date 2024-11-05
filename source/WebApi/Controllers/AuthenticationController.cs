3f809cd104c661e9de39d28787bb233fbaae04ea
Leonardo Pereira <leonardobrpereira2015@gmail.com>
2024-11-04 21:30:41 -0300

    criando rota para retornar todos os usuarios

--- source/WebApi/Controllers/AuthenticationController.cs
+++ source/WebApi/Controllers/AuthenticationController.cs
@@ -4,6 +4,7 @@ using Project.Application.Features.Commands.RegisterUser;
 using Project.Application.Features.Commands.LoginUser;
 using Project.Domain.Notifications;
 using Microsoft.AspNetCore.Authorization;
+using Project.Application.Features.Queries.GetAllUsers;
 
 namespace Project.WebApi.Controllers
 {
@@ -26,6 +27,15 @@ namespace Project.WebApi.Controllers
         public async Task<IActionResult> Login([FromBody] LoginUserCommandRequest request)
         {
             return Response(await _mediatorHandler.Send(new LoginUserCommand(request)));
+        }
+
+        [HttpGet("GetAllUsers")]
+        [ProducesResponseType(typeof(GetAllUsersQueryResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllUsers([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10, [FromQuery] string? filter = null)
+        {
+            var query = new GetAllUsersQuery(pageNumber, pageSize, filter);
+            var result = await _mediatorHandler.Send(query);
+            return Response(result);
         }
     }
 }
