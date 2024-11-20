57984b9bbae01b138cad90e560eed26981ea5d7a
Leonardo Pereira <leonardobrpereira2015@gmail.com>
2024-11-20 13:21:23 -0300

    feat: criando rota GET para parametrizacao

--- source/WebApi/Controllers/ParametrizacaoController.cs
+++ source/WebApi/Controllers/ParametrizacaoController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Project.Domain.Notifications;
 using Microsoft.AspNetCore.Authorization;
 using Project.Application.Features.Commands.CreateParametrizacao;
-// using Project.Application.Features.Queries.GetAllParametrizacao;
+using Project.Application.Features.Queries.GetAllParametrizacao;
 // using Project.Application.Features.Queries.GetParametrizacaoById;
 // using Project.Application.Features.Commands.UpdateParametrizacao;
 // using Project.Application.Features.Commands.DeleteParametrizacao;
@@ -23,17 +23,17 @@ public class ParametrizacaoController(INotificationHandler<DomainNotification> n
         return Response(await _mediatorHandler.Send(new CreateParametrizacaoCommand(request)));
     }
 
-    // [Authorize(Roles = "Admin, User")]
-    // [HttpGet]
-    // [ProducesResponseType(typeof(GetAllParametrizacaoQueryResponse), StatusCodes.Status200OK)]
-    // public async Task<IActionResult> GetAllParametrizacao(
-    //     [FromQuery] int pageNumber = 1,
-    //     [FromQuery] int pageSize = 7,
-    //     [FromQuery] string? nomeVendedor = null)
-    // {
-    //     var query = new GetAllParametrizacaoQuery(pageNumber, pageSize, nomeVendedor);
-    //     return Response(await _mediatorHandler.Send(query));
-    // }
+    [Authorize(Roles = "Admin, User")]
+    [HttpGet]
+    [ProducesResponseType(typeof(GetAllParametrizacaoQueryResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllParametrizacao(
+        [FromQuery] int pageNumber = 1,
+        [FromQuery] int pageSize = 7,
+        [FromQuery] string? nomeVendedor = null)
+    {
+        var query = new GetAllParametrizacaoQuery(pageNumber, pageSize, nomeVendedor);
+        return Response(await _mediatorHandler.Send(query));
+    }
 
     // [Authorize(Roles = "Admin, User")]
     // [HttpGet("{id}")]
